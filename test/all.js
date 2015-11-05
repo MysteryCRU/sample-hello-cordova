c75e91aee1388bbc7030a8010c54ec958d625294
Anil <anil@pcp114981pcs.wireless.calpoly.edu>
2015-11-05 11:43:10 -0800

    test/all.js

---
+++ test/all.js
@@ -0,0 +1,9 @@
+var assert = require('assert')
+exports['test if the values are the same'] = function() {
+    //var util = require("../www/js/util.js");
+    //var testObj = {"lol":"ok"};
+    //var testResult = util.copyObject(testObj);
+    assert.equal(2 + 3, 5, 'four is not equal to five')
+}
+ 
+if (module == require.main) require('test').run(exports)
